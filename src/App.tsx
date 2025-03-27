3207e7f92dc1cba46eeb17534c30695c8e6bfa66
Mark Paul <markpaulbusiness66@gmail.com>
2025-03-27 18:51:02 +0300

    outlined key features of the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import { useState } from 'react'
 import './App.css'
 
+// todo1: capture user input through a form inputs.
+// todo2: display the user input.
+// todo3: enable delete feature.
+// todo4: enable update edit feature.
+
+
 function App() {
 
   return (
