00f1555278dd413e0d2e66d4d7f208a919653831
Mark Paul <markpaulbusiness66@gmail.com>
2025-03-28 10:24:41 +0300

    We can now display our tasks.

--- src/App.tsx
+++ src/App.tsx
@@ -8,17 +8,33 @@ import './App.css'
 
 function App() {
   const [task, setTask] = useState('')
+  const [tasks, setTasks] =  useState<string[]>([])
+  
+  function addTask(){
+    const tasksClone: string[] = [...tasks]
+
+    tasksClone.push(task)
+
+    setTasks([...tasksClone])
+  }
+
   return (
     <div className='flex justify-center w-1/2 m-auto'>
       <div className=" flex-col gap-3 rounded-3xl p-3 justify-center w-2/5 bg-amber-100 ">
-        <h1 className=' text-2xl'>Todo List App</h1>
+        <h1 className=' text-2xl p-3'>Todo List App</h1>
      <input type="text"
           value={task}
           placeholder='Enter your task'
           onChange={(e)=>{setTask(e.target.value)}}
          />
+      <button onClick={addTask}>Add Task</button>
 
-         
+      <ul>
+        {tasks.map(t=>(
+          <li className='' key={t}>{t}</li>
+        ))}
+      </ul>
+
 
       </div>
     </div>
