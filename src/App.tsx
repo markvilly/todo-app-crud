8616cadba6dbef76c71d549eb8ccb7fb4a26bcd3
Mark Paul <markpaulbusiness66@gmail.com>
2025-03-28 10:02:35 +0300

    added imported tailwind css and input field

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,22 @@ import './App.css'
 // todo3: enable delete feature.
 // todo4: enable update edit feature.
 
-
 function App() {
-
+  const [task, setTask] = useState('')
   return (
-    <>
-      
-    </>
+    <div className='flex justify-center w-1/2 m-auto'>
+      <div className=" flex-col gap-3 rounded-3xl p-3 justify-center w-2/5 bg-amber-100 ">
+        <h1 className=' text-2xl'>Todo List App</h1>
+     <input type="text"
+          value={task}
+          placeholder='Enter your task'
+          onChange={(e)=>{setTask(e.target.value)}}
+         />
+
+         
+
+      </div>
+    </div>
   )
 }
 
