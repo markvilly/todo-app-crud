50451a9ef7c60dc878619de012a531c8c34122f6
Mark Paul <markpaulbusiness66@gmail.com>
2025-03-28 11:26:46 +0300

    Added Delete functionality

--- src/App.tsx
+++ src/App.tsx
@@ -14,24 +14,30 @@ function App() {
     const tasksClone: string[] = [...tasks]
 
     tasksClone.push(task)
-
     setTasks([...tasksClone])
+    setTask('')
   }
 
   return (
     <div className='flex justify-center w-1/2 m-auto'>
-      <div className=" flex-col gap-3 rounded-3xl p-3 justify-center w-2/5 bg-amber-100 ">
+      <div className=" flex-col rounded-3xl p-3 justify-center w-3/5 bg-amber-100 ">
         <h1 className=' text-2xl p-3'>Todo List App</h1>
+     <div className="flex justify-center pb-6">
      <input type="text"
+          className=' bg-amber-50 p-1 mr-2 rounded'
           value={task}
           placeholder='Enter your task'
           onChange={(e)=>{setTask(e.target.value)}}
          />
-      <button onClick={addTask}>Add Task</button>
+      <button className=' bg-amber-600 p-2 rounded text-amber-50' onClick={addTask}>Add Task</button>
+     </div>
 
       <ul>
         {tasks.map(t=>(
-          <li className='' key={t}>{t}</li>
+          <div className='flex  justify-between border-b-1 border-amber-400'>
+            <li className=' flex pl-5' key={t}>{t}</li>
+            <button>🚮</button>
+          </div>
         ))}
       </ul>
 
